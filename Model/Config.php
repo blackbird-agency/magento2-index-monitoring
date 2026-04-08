2f183ea67c8ea63e57ce328f9235bddabe7e19aa
Matthieu Miloud <matthieu@bird.eu>
2026-04-08 09:14:45 +0200

    refactor: remove hardcoded fallback as we have a config.xml settings those already

--- Model/Config.php
+++ Model/Config.php
@@ -11,6 +11,7 @@ class Config
     public const XML_PATH_ENABLED = 'blackbird_index_monitoring/general/enabled';
     public const XML_PATH_RECIPIENTS = 'blackbird_index_monitoring/general/recipients';
     public const XML_PATH_THRESHOLD_MINUTES = 'blackbird_index_monitoring/general/threshold_minutes';
+    public const XML_PATH_IDLE_PENDING_THRESHOLD_MINUTES = 'blackbird_index_monitoring/general/idle_pending_threshold_minutes';
 
     public function __construct(private readonly ScopeConfigInterface $scopeConfig)
     {
@@ -28,7 +29,11 @@ class Config
 
     public function getThresholdMinutes(): int
     {
-        $val = (int) ($this->scopeConfig->getValue(self::XML_PATH_THRESHOLD_MINUTES, ScopeInterface::SCOPE_STORE) ?? 60);
-        return $val > 0 ? $val : 60;
+        return (int) ($this->scopeConfig->getValue(self::XML_PATH_THRESHOLD_MINUTES, ScopeInterface::SCOPE_STORE));
+    }
+
+    public function getIdlePendingThresholdMinutes(): int
+    {
+        return (int) ($this->scopeConfig->getValue(self::XML_PATH_IDLE_PENDING_THRESHOLD_MINUTES, ScopeInterface::SCOPE_STORE));
     }
 }
